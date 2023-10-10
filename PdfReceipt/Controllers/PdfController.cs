0626e79d0fb20c95ab52521c413a1bbfd219dd29
Anthonia Okwe <anthonia_okwe@yahoo.com>
2023-10-10 18:47:26 +0100

    Add project files.

---
+++ PdfReceipt/Controllers/PdfController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PdfReceipt.Services;
+
+namespace PdfReceipt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : Controller
+    {
+        private readonly PdfService _pdfService;
+        public PdfController(PdfService pdfService)
+        {
+            _pdfService = pdfService;
+        }
+
+        [HttpPost]
+        [Route("GeneratePdf")]
+        public IActionResult GeneratePdf([FromBody] string htmlContent)
+        {
+            byte[] pdfBytes = _pdfService.GeneratePdf(htmlContent);
+            return File(pdfBytes, "application/pdf");
+        }
+    }
+}
---
+++ PdfReceipt/Services/PdfService.cs
@@ -0,0 +1,16 @@
+﻿using SelectPdf;
+
+namespace PdfReceipt.Services
+{
+    public class PdfService
+    {
+        public byte[] GeneratePdf(string htmlContent)
+        {
+            HtmlToPdf converter = new HtmlToPdf();
+            PdfDocument document = converter.ConvertHtmlString(htmlContent);
+            byte[] pdfBytes = document.Save();
+            document.Close();
+            return pdfBytes;
+        }
+    }
+}
